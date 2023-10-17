69c427557383ef508e2a4077a888e9bfc14a0cbd
iamnevir <hiromansofaoi@gmail.com>
2023-10-17 15:47:05 +0700

    fixed

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -5,7 +5,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import { BillBoardColumn } from "./columns";
+import { CategoryColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { toast } from "react-hot-toast";
@@ -14,7 +14,7 @@ import { useState } from "react";
 import axios from "axios";
 import { AlertModal } from "@/components/modals/alert-modal";
 interface CellActionProps {
-  data: BillBoardColumn;
+  data: CategoryColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -26,18 +26,18 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard Id copied to the clipboard.");
+    toast.success("Category Id copied to the clipboard.");
   };
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
       router.refresh();
-      router.push(`/${params.storeId}/billboards`);
-      toast.success("Billboard deleted.");
+      router.push(`/${params.storeId}/categories`);
+      toast.success("Category deleted.");
     } catch (error) {
       toast.error(
-        "Make sure you removed all categories using this billboard first"
+        "Make sure you removed all product using this Category first"
       );
     } finally {
       setLoading(false);
